9cb43edf59c21e61631cee46fe31422ec991267a
rom013 <romullomelo0130@gmail.com>
2023-12-10 21:04:08 -0300

    feat: connect menu with api

--- src/components/menu/menuForum.jsx
+++ src/components/menu/menuForum.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react"
 import LogoForum from "../../assets/img/logoForum.png"
 import { ButtonAddNewForum, ButtonNavigateForum, ButtonProfile } from "../buttons/button"
+import { createClient } from "@supabase/supabase-js"
 
 export default function MenuForum() {
+
+    const supabase = createClient(import.meta.env.VITE_URL_SUPABASE, import.meta.env.VITE_API_KEY_SUPABASE)
+
+    const [myForum, setForum] = useState([])
+
+    useEffect(() => {
+        supabase
+            .from('forum')
+            .select('id, name_forum, img_forum')
+            .eq("active", true)
+            .then(({data}) => {
+                setForum(data)
+            })
+
+
+        // if (error) {
+        //     console.error(error);
+        //     return;
+        // }
+
+    }, [])
+
     return (
         <article
             className="py-10 px-4 bg-zinc-800 h-screen flex flex-col justify-between"
@@ -9,11 +33,16 @@ export default function MenuForum() {
             <div
                 className="flex flex-col gap-3"
             >
-                <ButtonNavigateForum
-                    logoForum={LogoForum}
-                />
+                {
+                    myForum.map(forum => {
+                        return <ButtonNavigateForum
+                            logoForum={forum.img_forum}
+                        />
+                    })
+                }
 
-                <ButtonAddNewForum/>
+
+                <ButtonAddNewForum />
             </div>
 
             <ButtonProfile
